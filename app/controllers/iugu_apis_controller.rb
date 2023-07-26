d5e8e6b4ddf9f5ec3230bd65b6cbbf0e0e90b93c
Erick Takeshi <erick.rezende@iugu.com>
2023-07-26 10:27:42 -0300

    add iugu apis

---
+++ app/controllers/iugu_apis_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class IuguApisController < ApplicationController
+  IUGU_WORKSPACES_URL = 'https://api.console.iugu.test'
+
+  def index
+    @auth_code_flow = AuthCodeFlow.find(session[:auth_code_flow_id])
+  end
+
+  def get_workspaces
+    auth_code_flow = AuthCodeFlow.find(session[:auth_code_flow_id])
+    conn = Faraday.new(IUGU_WORKSPACES_URL, ssl: { verify: false }) do |f|
+      f.request :authorization, 'Bearer', auth_code_flow.access_token
+    end
+
+    response = conn.get('/workspaces')
+    parsed_response = JSON.parse(response.body)
+
+    @workspace_response = parsed_response
+
+    render :index
+  end
+end
